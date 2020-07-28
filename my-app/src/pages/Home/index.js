998d82a83ab2af15119effa9b64002adb4e176c3
Felipe Moura <felipemimoura@gmail.com>
2020-07-28 16:48:46 -0300

    segundaAula
    Segunda Aula da imersão alura react

--- my-app/src/pages/Home/index.js
+++ my-app/src/pages/Home/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import Menu from './components/Menu/';
-import dadosIniciais from './data/dados_iniciais.json';
-import BannerMain from './components/BannerMain';
-import Carousel from './components/Carousel';
-import Footer from './components/Footer';
+import Menu from '../../components/Menu';
+import dadosIniciais from '../../data/dados_iniciais.json';
+import BannerMain from '../../components/BannerMain';
+import Carousel from '../../components/Carousel';
+import Footer from '../../components/Footer';
 
-function App() {
+function Home() {
   return (
     <div style={{ background: '#141414' }}>
       <Menu />
       <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={'Hotsite sobre TLOUS PART Ii'}
+        videoDescription={'Hotsite sobre TLOUS PART II'}
       />
 
       <Carousel ignoreFirstVideo category={dadosIniciais.categorias[0]} />
@@ -32,4 +32,4 @@ function App() {
   );
 }
 
-export default App;
+export default Home;
